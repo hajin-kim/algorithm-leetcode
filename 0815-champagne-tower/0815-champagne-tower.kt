2bb5104c17e25460d499c6d0236287fc1812ac50
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-09-25 07:23:31 +0900

    Time: 136 ms (100%), Space: 37 MB (75%) - LeetHub

---
+++ 0815-champagne-tower/0815-champagne-tower.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun champagneTower(poured: Int, query_row: Int, query_glass: Int): Double {
+        var dp = DoubleArray(query_row + 1)
+        dp[0] = poured.toDouble()
+
+        repeat(query_row) { row ->
+            val newDp = DoubleArray(query_row + 1)
+
+            repeat(row + 1) { column ->
+                // floating point error?
+                val overflowAmount = (dp[column] - 1.0).coerceAtLeast(0.0)
+                newDp[column] += overflowAmount / 2
+                newDp[column + 1] += overflowAmount / 2
+            }
+
+            dp = newDp
+        }
+
+        val result = dp[query_glass].coerceAtMost(1.0)
+        return result
+    }
+}
