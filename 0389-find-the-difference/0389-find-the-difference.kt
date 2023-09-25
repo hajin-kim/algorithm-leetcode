6ce803de94d9f3fd99ab9a31cc229510db4328be
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-09-26 04:03:58 +0900

    Time: 139 ms (92.31%), Space: 34.6 MB (78.85%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.kt
+++ 0389-find-the-difference/0389-find-the-difference.kt
@@ -2,13 +2,15 @@ class Solution {
     fun findTheDifference(s: String, t: String): Char {
         val count = IntArray(26)
 
-        s.forEach { count[it - 'a']++ }
-        t.forEach {
+        for (it in s) {
+            count[it - 'a']++
+        }
+        for (it in t) {
             count[it - 'a']--
             if (count[it - 'a'] < 0)
                 return it
         }
-        
+
         return '-'
     }
 }
