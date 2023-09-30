1b408d4003241edb43d45367fd0470f9583765b9
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-09-30 23:52:46 +0900

    Time: 385 ms (62.5%), Space: 66.9 MB (12.5%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.kt
+++ 0456-132-pattern/0456-132-pattern.kt
@@ -2,23 +2,19 @@ import java.util.*
 
 class Solution {
     fun find132pattern(nums: IntArray): Boolean {
-        var min = Int.MAX_VALUE
-        val ranges: Stack<IntRange> = Stack()
+        var two = Int.MIN_VALUE
+        val decreasingStack: Stack<Int> = Stack()
 
-        for (num in nums) {
-            if (ranges.any { num in it })
+        for (i in nums.indices.reversed()) {
+            val num = nums[i]
+            if (num < two)
                 return true
 
-            min = minOf(min, num)
-            if (num > min) {
-                while (ranges.isNotEmpty() && ranges.peek().endExclusive < num) {
-                    ranges.pop()
-                }
-                ranges.add(min + 1..<num)
+            while (decreasingStack.isNotEmpty() && decreasingStack.peek() < num) {
+                two = decreasingStack.pop()
             }
+            decreasingStack.push(num)
         }
-
-
 
         return false
     }
