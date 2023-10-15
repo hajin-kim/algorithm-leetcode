08d91517b798e63649178da019a3347ffbbd2782
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-10-15 13:46:37 +0900

    Time: 2981 ms (12.5%), Space: 40.7 MB (50%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.kt
@@ -0,0 +1,39 @@
+class Solution {
+    private val MODULO = (1e9 + 7).toInt()
+    fun numWays(steps: Int, arrLen: Int): Int {
+        if (arrLen == 1)
+            return 1
+
+        var prev = IntArray(arrLen) { 0 }
+            .apply {
+                this[0] = 1
+                this[1] = 1
+            }
+
+        var new = IntArray(arrLen)
+
+        for (step in 1 until steps) {
+            for (i in 0 until arrLen) {
+                // Stay
+                var result = prev[i]
+                // Right
+                if (i + 1 < arrLen) {
+                    result += prev[i + 1]
+                    result %= MODULO
+                }
+                // Left
+                if (i - 1 >= 0) {
+                    result += prev[i - 1]
+                    result %= MODULO
+                }
+                new[i] = result
+            }
+
+            val temp = new
+            new = prev
+            prev = temp
+        }
+
+        return prev[0]
+    }
+}
