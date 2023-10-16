18253c95fe8473e607e4602ec2d7830dd567336b
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-10-16 09:56:21 +0900

    Time: 130 ms (95.52%), Space: 35 MB (43.28%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.kt
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.kt
@@ -1,21 +1,7 @@
 class Solution {
 
-    fun getRow(rowIndex: Int): List<Int> {
-        var prev = IntArray(rowIndex + 3)
-        prev[0] = 0
-        prev[1] = 1
-        prev[2] = 0
-        var next = IntArray(rowIndex + 3)
-
-        for (i in 0 until rowIndex) {
-            for (j in 1..i + 2)
-                next[j] = prev[j - 1] + prev[j]
-
-            val temp = prev
-            prev = next
-            next = temp
+    fun getRow(rowIndex: Int): List<Int> =
+        (1L..rowIndex).scan(1) { prev: Int, k: Long ->
+            (prev * (rowIndex - k + 1) / k).toInt()
         }
-
-        return prev.slice(1 until rowIndex + 2)
-    }
 }
