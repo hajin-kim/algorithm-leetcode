c8434c7cc079c9d9b5b48fea9df406a53fe939eb
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-10-23 12:55:49 +0900

    Time: 137 ms (67.65%), Space: 33.3 MB (86.76%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.kt
+++ 0342-power-of-four/0342-power-of-four.kt
@@ -1,5 +1,4 @@
 class Solution {
-    fun isPowerOfFour(n: Int): Boolean = n > 0 &&
-            n.countOneBits() == 1 &&
+    fun isPowerOfFour(n: Int): Boolean = n.countOneBits() == 1 &&
             n.countTrailingZeroBits() % 2 == 0
 }
