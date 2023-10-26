f6affd83bdfaa3292b11fb458f646b509fd00c74
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-10-26 21:43:55 +0900

    Time: 396 ms (50%), Space: 40.7 MB (50%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.kt
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.kt
@@ -2,30 +2,32 @@ class Solution {
     private val MODULO = 1000000007
 
     fun numFactoredBinaryTrees(arr: IntArray): Int {
-        val sortedArr = arr.sortedArray()
+        val sortedArr = LongArray(arr.size) { arr[it].toLong() }
+            .sortedArray()
         val cache = sortedArr
-            .associate { it.toLong() to 1 }
+            .associateWith { 1L }
             .toMutableMap()
 
-        for (i in sortedArr.indices) {
-            for (j in 0..i) {
-                val iValue = sortedArr[i].toLong()
-                val jValue = sortedArr[j].toLong()
-                val product = iValue * jValue
+        for (i in sortedArr) {
+            for (j in sortedArr) {
+                val product = i * j
 
                 if (product in cache) {
-                    val orderFactor = if (i == j) 1L else 2L
+                    val orderFactor = if (i == j) 1 else 2
 
-                    val new = (orderFactor
-                            * cache[iValue]!!
-                            * cache[jValue]!!
-                            + cache[product]!!) % MODULO
-                    cache[product] = new.toInt()
+                    cache[product] = (orderFactor
+                            * cache[i]!!
+                            * cache[j]!!
+                            + cache[product]!!)
                 }
+
+                if (i == j)
+                    break
             }
         }
 
         return cache.values
-            .fold(0) { acc, i -> (acc + i) % MODULO }
+            .fold(0L) { acc, i -> (acc + i) % MODULO }
+            .toInt()
     }
 }
