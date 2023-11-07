b5fa9afe9a6d31ea292da2c5063cc05889ee9e5a
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-07 12:17:12 +0900

    Time: 706 ms (100%), Space: 65 MB (100%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.kt
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.kt
@@ -1,11 +1,9 @@
 class Solution {
-    fun eliminateMaximum(dist: IntArray, speed: IntArray): Int {
-        return dist
-            .zip(speed)
-            .map { (it.first - 1) / it.second }
-            .sorted()
-            .withIndex()
-            .takeWhile { (index, time) -> time >= index }
-            .size
-    }
+    fun eliminateMaximum(dist: IntArray, speed: IntArray): Int = dist
+        .zip(speed)
+        .map { (dist, speed) -> (dist - 1) / speed }
+        .sorted()
+        .withIndex()
+        .takeWhile { (index, time) -> time >= index }
+        .size
 }
