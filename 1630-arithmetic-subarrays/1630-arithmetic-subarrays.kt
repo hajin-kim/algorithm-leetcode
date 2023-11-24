6d64de5acff3bda04ea6d5b81e9510815998e29b
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-24 17:29:54 +0900

    Time: 313 ms (100.00%), Space: 41.4 MB (42.86%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.kt
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.kt
@@ -1,12 +1,29 @@
 class Solution {
-    private fun List<Int>.isArithmetic(): Boolean =
-        this.asSequence()
-            .sorted()
-            .zipWithNext()
-            .map { (prev, next) -> next - prev }
-            .zipWithNext()
-            .all { (prevDiff, nextDiff) -> prevDiff == nextDiff }
+    private fun List<Int>.isArithmetic(): Boolean {
+        val min = min()
+        val max = max()
 
+        if (min == max)
+            return true
+
+        if ((max - min) % (size - 1) != 0)
+            return false
+        val step = (max - min) / (size - 1)
+
+        val visited = BooleanArray(size + 1)
+
+        for (num in this) {
+            if ((num - min) % step != 0)
+                return false
+            val position = (num - min) / step
+
+            if (visited[position])
+                return false
+            visited[position] = true
+        }
+
+        return true
+    }
 
     fun checkArithmeticSubarrays(nums: IntArray, l: IntArray, r: IntArray): List<Boolean> =
         (l zip r).map { (left, right) ->
