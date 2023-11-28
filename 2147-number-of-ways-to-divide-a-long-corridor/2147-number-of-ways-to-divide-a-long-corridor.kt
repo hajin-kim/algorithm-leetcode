d0c769b0da39901cfdb5790d7e679c2d86507465
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-11-28 12:36:15 +0900

    Time: 520 ms (20.00%), Space: 50.9 MB (60.00%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.kt
@@ -0,0 +1,24 @@
+class Solution {
+    private val MODULO = (1e9 + 7).toInt()
+    
+    fun numberOfWays(corridor: String): Int {
+        val seatIndices = corridor
+            .withIndex()
+            .filter { it.value == 'S' }
+            .map { it.index }
+
+        if (seatIndices.size < 2 || seatIndices.size % 2 == 1)
+            return 0
+
+        var result = 1L
+        for (i in 1 until seatIndices.size / 2) {
+            val nextStart = i * 2
+            val previousEnd = nextStart - 1
+
+            result *= seatIndices[nextStart] - seatIndices[previousEnd]
+            result %= MODULO
+        }
+
+        return result.toInt()
+    }
+}
