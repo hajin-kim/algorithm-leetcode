e48cc915751dccff09c64b4c05d4f1fad206e2b5
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-12-25 04:44:11 +0900

    Time: 161 ms (88.89%), Space: 37.1 MB (77.78%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.kt
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.kt
@@ -1,16 +1,13 @@
 class Solution {
     fun minOperations(s: String): Int {
         var even = 0
-        var odd = 0
 
         for (i in s.indices) {
             val char = s[i]
             if ((char == '0') == (i % 2 == 0))
                 ++even
-            else
-                ++odd
         }
 
-        return minOf(even, odd)
+        return minOf(even, s.length - even)
     }
 }
