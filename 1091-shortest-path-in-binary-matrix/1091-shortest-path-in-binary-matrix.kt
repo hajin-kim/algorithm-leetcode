c395e200a21e04a98c86526f67532dc951f9455b
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-03-31 09:55:47 +0900

    Time: 567 ms (16.44%), Space: 57.8 MB (10.28%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.kt
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.kt
@@ -1,4 +1,5 @@
-import java.util.PriorityQueue
+import java.util.LinkedList
+import java.util.Queue
 
 data class Point(
     val r: Int,
@@ -6,59 +7,58 @@ data class Point(
 )
 
 class Solution {
-    private val UNVISITED = 1_000_000_000
-    fun dijkstra(grid: Array<IntArray>): Int {
+    private fun bfs(grid: Array<IntArray>): Int {
         val n = grid.size
         val start = Point(r = 0, c = 0)
 
         fun Point.isInRange(): Boolean =
             r in 0 until n && c in 0 until n
 
-        val priorityQueue = PriorityQueue<Pair<Int, Point>>(compareBy { it.first })
+        fun Point.atEnd(): Boolean =
+            r == n - 1 && c == n - 1
+
+        val queue: Queue<Point> = LinkedList()
         val visited = Array(n) { BooleanArray(n) }
-        val distances = Array(n) { IntArray(n) { UNVISITED } }
 
-        priorityQueue.add(1 to start)
-        distances[0][0] = 1
+        queue.add(start)
 
-        while (priorityQueue.isNotEmpty()) {
-            val (cost, point) = priorityQueue.poll()
-            val (r, c) = point
+        var distance = 1
+        while (queue.isNotEmpty()) {
+            repeat(queue.size) {
+                val point = queue.poll()
+                val (r, c) = point
 
-            if (visited[r][c])
-                continue
-            visited[r][c] = true
+                if (visited[r][c])
+                    return@repeat
+                if (point.atEnd())
+                    return distance
+                visited[r][c] = true
 
-            val neighbors = listOf(
-                1 to 1,
-                1 to 0,
-                1 to -1,
-                0 to 1,
-                0 to -1,
-                -1 to 1,
-                -1 to 0,
-                -1 to -1,
-            )
-                .map { (dr, dc) -> Point(r + dr, c + dc) }
-                .filter { it.isInRange() && grid[it.r][it.c] == 0 }
+                val neighbors = listOf(
+                    1 to 1,
+                    1 to 0,
+                    1 to -1,
+                    0 to 1,
+                    0 to -1,
+                    -1 to 1,
+                    -1 to 0,
+                    -1 to -1,
+                )
+                    .map { (dr, dc) -> Point(r + dr, c + dc) }
+                    .filter { it.isInRange() && grid[it.r][it.c] == 0 }
 
-            for (neighbor in neighbors) {
-                val newCost = cost + 1
-                if (newCost < distances[neighbor.r][neighbor.c]) {
-                    distances[neighbor.r][neighbor.c] = newCost
-                    priorityQueue.add(newCost to neighbor)
-                }
+                queue.addAll(neighbors)
             }
+            ++distance
         }
 
-        val endDistance = distances[n - 1][n - 1]
-        return if (endDistance == UNVISITED) -1 else endDistance
+        return -1
     }
 
     fun shortestPathBinaryMatrix(grid: Array<IntArray>): Int {
         if (grid[0][0] == 1)
             return -1
 
-        return dijkstra(grid)
+        return bfs(grid)
     }
 }
