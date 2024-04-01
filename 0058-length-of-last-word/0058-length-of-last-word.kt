ed264343a8189d3d3f3c9687eb7e5cbd6116f690
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-04-01 09:54:53 +0900

    Time: 133 ms (89.94%), Space: 36.5 MB (61.1%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.kt
+++ 0058-length-of-last-word/0058-length-of-last-word.kt
@@ -1,5 +1,11 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        return s.split(' ').last { it.isNotEmpty() }.length
+        var end = s.lastIndex
+        while (s[end] == ' ') --end
+
+        var start = end
+        while (start >= 0 && s[start] != ' ') --start
+        
+        return end - start
     }
 }
