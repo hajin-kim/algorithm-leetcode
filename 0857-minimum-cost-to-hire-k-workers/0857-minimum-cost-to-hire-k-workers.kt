7203a3dec391a5743504a6327e9df447a717bc71
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-05-11 19:51:28 +0900

    Time: 323 ms (20%), Space: 41 MB (60%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.kt
@@ -0,0 +1,40 @@
+import java.util.PriorityQueue
+
+class Solution {
+    fun mincostToHireWorkers(quality: IntArray, wage: IntArray, k: Int): Double {
+        val n = quality.size
+
+        val rates = DoubleArray(n) { wage[it].toDouble() / quality[it] }
+
+        val qualityPriorityQueue = PriorityQueue<Int>(k, reverseOrder())
+
+        val indices = (0 until n)
+            .sortedWith(compareBy<Int> { rates[it] }.thenBy { wage[it] })
+            .toIntArray()
+
+        var result = Double.MAX_VALUE
+        var sumQuality = 0
+
+        for (i in 0 until k - 1) {
+            val now = indices[i]
+
+            qualityPriorityQueue += quality[now]
+            sumQuality += quality[now]
+        }
+
+        for (i in k - 1 until n) {
+            val now = indices[i]
+
+            qualityPriorityQueue += quality[now]
+            sumQuality += quality[now]
+
+            val cost = rates[now] * sumQuality
+            result = minOf(result, cost)
+
+            val currentMaxQuality = qualityPriorityQueue.poll()
+            sumQuality -= currentMaxQuality
+        }
+
+        return result
+    }
+}
